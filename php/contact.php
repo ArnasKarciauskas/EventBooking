5f6a4516ec6d7d25c1b0ce3daa6f7d110958de8b
Arnas Karciauskas <arnaskarciauskas@gmail.com>
2017-12-10 15:13:44 +0000

    second commit

--- php/contact.php
+++ php/contact.php
@@ -1,26 +1,29 @@
 <?php
 $from ="arnaskarciauskas@gmail.com";
 $to = "arnaskarciauskas@gmail.com";
+if (isset($_POST['submit'])) {
 	  $name = $_POST['name'];
       $email = $_POST['email'];
 	  $phone = $_POST['phone'];
       $textarea = $_POST['textarea'];
 	  $promotional = $_POST['promotional'];
+	  
 $body .="Name: ";
 $body .=$name;
-$body .="E=mail ";
+$body .="E-mail ";
 $body .=$email;
 $body .="Phone: ";
 $body .=$phone;
-$body .="Textarea :";
+$body .="Message :";
 $body .=$textarea;
 $body .="Promotional?";
 $body .=$promo;
 
 $go = mail($to, $subject, $body, "From:<$from>");
 
+}
 if($go){
-		print("Success!");
+	rint("Success!");
 }
 else{
 	print("Unable to send");
