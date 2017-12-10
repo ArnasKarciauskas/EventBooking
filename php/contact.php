baded547dcf11e501701b86d01c3509dff6fd9be
Arnas Karciauskas <arnaskarciauskas@gmail.com>
2017-12-10 15:18:25 +0000

    third commit

--- php/contact.php
+++ php/contact.php
@@ -1,32 +1,30 @@
 <?php
-$from ="arnaskarciauskas@gmail.com";
-$to = "arnaskarciauskas@gmail.com";
 if (isset($_POST['submit'])) {
 	  $name = $_POST['name'];
       $email = $_POST['email'];
 	  $phone = $_POST['phone'];
       $textarea = $_POST['textarea'];
-	  $promotional = $_POST['promotional'];
-	  
-$body .="Name: ";
-$body .=$name;
-$body .="E-mail ";
-$body .=$email;
-$body .="Phone: ";
-$body .=$phone;
-$body .="Message :";
-$body .=$textarea;
-$body .="Promotional?";
-$body .=$promo;
+	  $promotional = $_POST['promotional'];     
+      
+      //Load previous XML file
+      $xml = simplexml_load_file("./xmld/contact.xml") or die("ERROR: Cannot load contact.xml.");
 
-$go = mail($to, $subject, $body, "From:<$from>");
+      //Add a new node
+      $feedback = $xml->addChild('enquiry');
 
-}
-if($go){
-	rint("Success!");
-}
-else{
-	print("Unable to send");
+      //Add the details from forms to XML file
+	  $feedback->addChild('name', $name);
+      $feedback->addChild('email', $email);
+	  $feedback->addChild('phone', $phone);
+      $feedback->addChild('textarea', $textarea);
+	  $feedback->addChild('promotional', $promotional);   
+
+       //Save the new XML file
+      $xml->asXml('./xmld/contact.xml');     
+
+      header("Location: ../thankyou.html");
+} else {
+      header("Location: ../index.html");
 }
 
 ?>
